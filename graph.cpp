eae4de03bed406a3eb8e7c45e85c7b6d6e60968b
Alexy de Almeida <alexx.da95@gmail.com>
2022-04-18 15:59:46 +0100

    200 points

--- graph.cpp
+++ graph.cpp
@@ -7,20 +7,19 @@ bool is_first_node;
 
 map<int, pair<vector<int>, int>> graph;
 vector<bool> visited;
+vector<bool> visited_stat3;
 vector<vector<int>> nodes;
 vector<vector<int>> parent_nodes;
 vector<int> nodes_order;
 vector<int> dp;
 vector<bool> is_on_stack;
 vector<int> bottlenecks;
-
-// set<int>white;
-// set<int>black;
+vector<int> bottleneck_count;
 
 int flag = 0;
-int bottleneck_count = 0;
 int num_last_nodes = 0;
 int best = 0;
+int node_bottleneck;
 
 bool check_last_node(){
 
@@ -36,11 +35,13 @@ bool check_last_node(){
             for (auto next_node: nodes[node]){
 
                 if (!visited[next_node] ) {
+                    // if the node hasn't been visited yet and doesn't have any following nodes, then it should be the last one
                     if (nodes[next_node].empty()) num_last_nodes++;
                     visited[next_node] = true;
                     Q.push(next_node);
                 }
 
+                // if there is more than 1 node that should be the last one, then it's impossible
                 if (num_last_nodes > 1) return false;
             }
         }
@@ -57,9 +58,10 @@ bool is_cyclic(int node){
         is_on_stack[node] = true;
 
         if(!nodes[node].empty()){
-            for (auto neighbor: nodes[node]){
-                if (!visited[neighbor] && is_cyclic(neighbor)) return true;
-                else if (is_on_stack[neighbor]) return true;
+            for (auto next_node: nodes[node]){
+                if (!visited[next_node] && is_cyclic(next_node)) return true;
+                // if the node already was on the stack, then it's pointing to a cycle
+                else if (is_on_stack[next_node]) return true;
             }
         }
     }
@@ -70,25 +72,12 @@ bool is_cyclic(int node){
 
 bool check_cycle(){
 
-    /*
-    white.erase(node);
-    black.insert(node);
-
-    if (!nodes[node].empty()){
-        for (int next_node: nodes[node]){
-            if (white.find(next_node) != white.end()) check_cycle(next_node);
-            if (black.find(next_node) != black.end()) flag = 1;
-        }
-    }
-
-    black.erase(node);
-    */
-
    for (int i = 1; i <= N; i++){
        if (is_cyclic(i)) return false;
    }
 
    return true;
+
 }
 
 void statistic1(){
@@ -109,27 +98,15 @@ void statistic1(){
 
         while (!pQ.empty()){
 
-            priority_queue <int, vector<int>, greater<int>> Q = pQ;
-
-            /*
-            cout << ":: ";
-            while (!Q.empty()){
-                cout << Q.top() << " <- ";
-                Q.pop();
-            }
-            cout << ":: " << endl;
-
-            */
-
             int node = pQ.top();
 
+            // flag to control if all dependencies have been solved
             int flagstat1 = 1;
 
             if (parent_nodes[node].size() > 1){
-
-
                 for (int parent: parent_nodes[node]){
-                    if (!visited[parent] || is_in_queue[parent]) {
+                    // if dependencies of actual node haven't been solved yet, I can't proceed with this one
+                    if (!visited[parent] || is_in_queue[parent]) {      
                         visited[node] = false;
                         is_in_queue[node] = false;
                         pQ.pop();
@@ -141,12 +118,14 @@ void statistic1(){
 
             if (flagstat1 == 0) continue;
 
+            // if all dependencies have been solved, then everything proceeds as normal
             min_amount_time += graph[node].second;
             nodes_order.push_back(node);
             is_in_queue[node] = false;
             pQ.pop();
 
             if (!nodes[node].empty()){
+                // if the next nodes haven't been visited yet, we're gonna put them in the queue
                 for (auto next_node: nodes[node]){
                     if (!visited[next_node]) {
                         pQ.push(next_node);
@@ -166,10 +145,11 @@ void statistic1(){
 
 void statistic2(int node){
 
+    // dp bottom-up approach
+
     visited[node] = true;
     dp[node] = graph[node].second;
     
-
     if (!nodes[node].empty()){
 
         for (int next_node: nodes[node]){
@@ -183,11 +163,43 @@ void statistic2(int node){
     return;
 }
 
+void search_parents(int node, int b_count){
+
+    // recursive function looking every parent of every parent... of this node
+
+    visited_stat3[node] = true;
+    bottleneck_count[b_count] ++;
+
+    for (auto parent: parent_nodes[node]){
+        if (!visited_stat3[parent]) {
+            search_parents(parent, b_count);
+        }
+    }
+}
+
+void search_children(int node, int b_count){
+
+    // recursive function looking every child of every child... of this node
+
+    visited_stat3[node] = true;
+    bottleneck_count[b_count] ++;
+
+    for (auto child: nodes[node]){
+        if (!visited_stat3[child]) {
+            search_children(child, b_count);
+        }
+    }
+
+}
+
 void statistic3(){
 
+    // reusing statistic1 function
+    // in statistic1 function we already control the right order of the bottlenecks
+
     vector <bool> is_in_queue(N+1);
-    bottlenecks.push_back(first_node);
     visited[first_node] = true;
+    bottlenecks.push_back(first_node);
 
     if (!nodes[first_node].empty()){
 
@@ -195,20 +207,9 @@ void statistic3(){
         for (auto next_node: nodes[first_node]) {
             visited[next_node] = true;
             is_in_queue[next_node] = true;
-            bottleneck_count++;
         }
 
         while (!pQ.empty()){
-
-            
-            priority_queue <int, vector<int>, greater<int>> Q = pQ;
-            cout << ":: ";
-            while (!Q.empty()){
-                cout << Q.top() << " <- ";
-                Q.pop();
-            }
-            cout << ":: " << endl;
-            
 
             int node = pQ.top();
             int flagstat1 = 1;
@@ -227,16 +228,29 @@ void statistic3(){
             }
 
             if (flagstat1 == 0) continue;
-
-            for (int parent: parent_nodes[node]){
-                if (visited[parent] && !is_in_queue[parent]) bottleneck_count --;
-            }
-
             is_in_queue[node] = false;
             pQ.pop();
 
-            cout << "[bottleneck count] " << bottleneck_count << endl;
-            if (bottleneck_count == 0 && pQ.empty()) bottlenecks.push_back(node);
+            //------------- stat3 algorithm here -------------//
+            // verify if the node can access to every node in the graph
+            // if it's possible to access all of them, then it's a bottleneck
+            visited_stat3 = vector<bool>(N+1, false);
+            visited_stat3[node] = true;
+
+            for (auto parent: parent_nodes[node]){
+                if (!visited_stat3[parent]) {
+                    search_parents(parent, node);
+                }
+            }
+            
+            for (auto child: nodes[node]){
+                if (!visited_stat3[child]) {
+                    search_children(child, node);
+                }
+            }
+
+            if (bottleneck_count[node] == N) bottlenecks.push_back(node);
+            //------------------------------------------//
 
             if (!nodes[node].empty()){
                 for (auto next_node: nodes[node]){
@@ -245,25 +259,21 @@ void statistic3(){
                         is_in_queue[next_node] = true;
                         visited[next_node] = true;
                     }
-                    bottleneck_count++;
                 }
             }
         }
     }
-
-    return;
 }
+
 int main(){
 
     cin >> N;
 
-    is_first_node = false;
-    visited = vector<bool>(N+1, false);
-    nodes = vector<vector<int>>(N+1);
-    parent_nodes = vector<vector<int>>(N+1);
-    is_on_stack =  vector<bool>(N+1, false);
-    
-    // for (int i = 1; i <= N; i++) white.insert(i);
+    is_first_node = false;                      // checks if a node is disconnected or if there's more than one initial node
+    visited = vector<bool>(N+1, false);         // list of nodes that have been visited
+    nodes = vector<vector<int>>(N+1);           // list of child nodes of every node
+    parent_nodes = vector<vector<int>>(N+1);    // list of parent nodes of every node
+    is_on_stack =  vector<bool>(N+1, false);    // used to check if there's a cycle
 
     for (int i = 1; i <= N; i++)
     {
@@ -275,7 +285,6 @@ int main(){
         {
             if (is_first_node)
             {
-                // cout << "[ERROR] too many first nodes or node is disconnected" << endl;
                 cout << "INVALID" << endl;
                 return 0;
             }
@@ -291,23 +300,22 @@ int main(){
             {
                 cin >> num_operation;
                 nodes[num_operation].push_back(i);
+                parent_nodes[i].push_back(num_operation);
+
                 graph[num_operation].first.push_back(i);
                 graph[i].first.push_back(num_operation);
-                parent_nodes[i].push_back(num_operation);
             }
         }
     }
 
     if (!check_last_node())
     {
-        // cout << "[ERROR] too many last nodes" << endl;
         cout << "INVALID" << endl;
         return 0;
     }
 
     visited = vector<bool>(N+1, false);
     if (!check_cycle()){
-        //cout << "[ERROR] cycle" << endl;
         cout << "INVALID" << endl;
         return 0;
     }
@@ -326,9 +334,11 @@ int main(){
             min_amount_time = 0;
             nodes_order = vector<int>();
             visited = vector<bool>(N+1, false);
-            parent_nodes[first_node].push_back(0);
-
-            if (statistic == 1) statistic1();
+            
+            if (statistic == 1) {
+                parent_nodes[first_node].push_back(0);
+                statistic1();
+            }
 
             else if (statistic == 2) {
                 dp = vector<int>(N+1);
@@ -339,8 +349,8 @@ int main(){
 
             else if (statistic == 3){
                 bottlenecks = vector<int>();
+                bottleneck_count = vector<int>(N+1, 1);
                 statistic3();
-
                 for (int bottleneck: bottlenecks) cout << bottleneck << endl;
                 return 0;
             }
